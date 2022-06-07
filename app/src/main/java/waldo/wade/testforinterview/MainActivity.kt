b9b869f0a4c25e8856961e348fcd101af882aca6
Wade <wadewaldo@gmail.com>
2022-06-07 15:53:10 +0800

    first commit

---
+++ app/src/main/java/waldo/wade/testforinterview/MainActivity.kt
@@ -0,0 +1,11 @@
+package waldo.wade.testforinterview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
